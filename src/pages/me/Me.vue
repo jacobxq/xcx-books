475257e807e337f7b2275be86cccd7a5908c72e7
jacobxq <zhihua0113@hotmail.com>
2018-06-29 21:12:41 +0800

    login

--- src/pages/me/Me.vue
+++ src/pages/me/Me.vue
@@ -45,42 +45,6 @@ export default {
     //     }
     //   })
     // },
-    getWxLogin: function ({encryptedData, iv, userinfo}) {
-      const self = this
-      wx.login({
-        success: function (loginResult) {
-          console.log('loginResult', loginResult)
-          var loginParams = {
-            code: loginResult.code,
-            encryptedData: encryptedData,
-            iv: iv
-          }
-          qcloud.setLoginUrl(config.loginUrl)
-          qcloud.requestLogin({
-            loginParams,
-            success () {
-              // 获取用户信息
-              qcloud.request({
-                url: config.userUrl,
-                login: true,
-                success (userRes) {
-                  showSuccess('登录成功')
-                  wx.setStorageSync('userinfo', userRes.data.data)
-                  self.userinfo = userRes.data.data
-                  console.log(userRes.data.data)
-                }
-              })
-            },
-            fail (error) {
-              showModal('登录失败', error)
-            }
-          })
-        },
-        fail: function (loginError) {
-          showModal('登录失败', loginError)
-        }
-      })
-    },
     login (e) {
       const self = this
       // 查看是否授权
@@ -89,38 +53,48 @@ export default {
           // 授权信息里有用户信息
           if (res.authSetting['scope.userInfo']) {
             // 检查用户登录是否过期
-            wx.checkSession({
-              success: function () {
-                // 没过期 直接成功
-                showSuccess('登录成功')
-              },
-              fail: function () {
-                // 过期了 重新登录 先清楚登录的状态
-                qcloud.clearSession()// 登录态已过期，需重新登录
-                // 登录需要的加密信息
-                var options = {
-                  encryptedData: e.mp.detail.encryptedData,
-                  iv: e.mp.detail.iv,
-                  userinfo: e.mp.detail.userInfo
+            const session = qcloud.Session.get()
+            qcloud.setLoginUrl(config.loginUrl)
+            if (session) {
+            // 第二次登录
+            // 或者本地已经有登录态
+            // 可使用本函数更新登录态
+              qcloud.loginWithCode({
+                success: res => {
+                  self.userinfo = res
+                  showSuccess('登录成功')
+                },
+                fail: err => {
+                  console.error(err)
+                  showModal('登录错误', err.message)
                 }
-                self.getWxLogin(options)
-              }
-            })
+              })
+            } else {
+              // 首次登录
+              qcloud.login({
+                success: res => {
+                  self.userinfo = res
+                  showSuccess('登录成功')
+                },
+                fail: err => {
+                  console.error(err)
+                  showModal('登录错误', err.message)
+                }
+              })
+            }
           } else {
             showModal('用户未授权', e.mp.detail.errMsg)
           }
         }
-      }) // let user = wx.getStorageSync('userinfo') // const self = this // if (!user) { // qcloud.setLoginUrl(config.loginUrl) // qcloud.login({ // success: function (userinfo) { // qcloud.request({ // url: config.userUrl, // login: true, // success (userRes) { // showSuccess('登录成功') // wx.setStorageSync('userinfo', userRes.data.data) // self.userinfo = userRes.data.data // } // }) // } // }) // }
+      })
     }
   },
   onShow () {
-    // console.log(123)
     let userinfo = wx.getStorageSync('userinfo')
     console.log([userinfo])
     if (userinfo) {
       this.userinfo = userinfo
     }
-    // console.log(this.userinfo)
   }
 }
 </script>
